4b43f4e2b77ccb7e885c00ca4ec5a16f584aab26
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-04-23 09:41:21 +0200

    Creating Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Container from 'react-bootstrap/Container'
+import Navigation from './components/Navigation'
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Container className='py-3'>
+
+      </Container>
       
     </div>
   );
